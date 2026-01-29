4dba52c1cd87e35daa11c13566970a24618a40c7
Christian Gleitzman <cg5n24@soton.ac.uk>
2026-01-29 15:02:47 +0000

    Initial working tex files

---
+++ src/common/bit_utils.h
@@ -0,0 +1,6 @@
+// src/common/bit_utils.h
+#ifndef BIT_UTILS_H
+#define BIT_UTILS_H
+
+// Macros will go here
+#endif
---
+++ src/week-01/example.c
@@ -0,0 +1,7 @@
+// src/week-01/example.c
+#include <stdio.h>
+
+int main(void) {
+    // Placeholder for Task 1
+    return 0;
+}
